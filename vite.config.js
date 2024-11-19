6ce47c2172a371c1f41c00b02a503457cceb6dc8
CyanCheetah <saitanuj12@gmail.com>
2024-11-19 17:55:01 -0600

    bhjk

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
   },
-};
-
+});
 
