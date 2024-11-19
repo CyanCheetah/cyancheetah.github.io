97b5e51e2ea0f6c186179d1c6c47bfee3ab85438
CyanCheetah <saitanuj12@gmail.com>
2024-11-19 17:20:20 -0600

    vercel

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
+export default {
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
   },
-});
+};
+
 
