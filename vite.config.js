d21862808219bf8020ecb547a4007db522828c8e
CyanCheetah <91763642+CyanCheetah@users.noreply.github.com>
2024-11-19 17:58:02 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
+  build: {
+    // Remove the externalization of react-router-dom for production
+    rollupOptions: {
+      // external: ['react-router-dom'], // Remove this line
+    },
   },
 });
-
